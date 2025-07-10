15b4589921713c99cd6a505bdfd3fcf1df36d922
melnig <ihormelnychenko7@gmail.com>
2025-07-10 16:48:51 +0300

    fix: fix routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import store from "./redux/store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
